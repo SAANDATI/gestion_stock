b31a55f7b813ea1d5c1e03c7f03a24f284e60ff6
saandati <saandatisalim@gmail.com>
2022-04-21 14:42:10 +0000

    CRUD JS

--- view/categorie/read.php
+++ view/categorie/read.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>Document</title>
+    <title>Gestion de stock</title>
 </head>
 <body>
     
