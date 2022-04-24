4e7baa8a2d5e0b96df800c59682c830e2b8f99f6
saandati <saandatisalim@gmail.com>
2022-04-24 16:35:55 +0000

    gestion des erreurs

--- service/DbConnection.php
+++ service/DbConnection.php
@@ -12,8 +12,8 @@
             if (self::$connexion==null) {
                 try {
                     // self::$connexion=new PDO('mysql:host=sql11.freemysqlhosting.net;dbname=sql11486821','sql11486821','TqIzAvCvEb');
-                    self::$connexion=new PDO("sqlite:../db.db");
-                    // self::$connexion=new PDO('mysql:host=localhost;dbname=gestion_stock','root','');
+                    // self::$connexion=new PDO("sqlite:../db.db");
+                    self::$connexion=new PDO('mysql:host=localhost;dbname=gestion_stock','root','');
                 } catch (PDOException $e) {
                     
                     echo"connection echouée".$e->getMessage();
